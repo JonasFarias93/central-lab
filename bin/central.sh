35de297d8aa4bb15f17e04e55831690a2952e0ca
JonasFarias93 <jhonas.jr@hotmail.com>
2026-05-24 11:47:36 -0300

    M5: adiciona comando backup com rotação de 5 arquivos

--- bin/central.sh
+++ bin/central.sh
@@ -74,6 +74,18 @@ atualizar_tudo() {
     atualizar_containers
 }
 
+fazer_backup(){
+    echo "=========================================="
+    echo " [Central] De Backup: mega "
+    echo "=========================================="
+    echo "⏳ Fazendo backup..."
+    mkdir -p ~/projects/central-lab/backups
+    ssh -p "$PORTA_MEGA" "${USER_MEGA}@${IP_MEGA}" "sudo tar -czf - /etc/ssh /etc/netplan /etc/ufw" > ~/projects/central-lab/backups/$(date +%F).tar.gz
+    ls -t ~/projects/central-lab/backups/*.tar.gz | tail -n +6 | xargs rm -f
+    echo "✅ Backup concluído"
+    echo "=========================================="
+}
+
 case "$1" in
     status)
         verificar_status
@@ -87,7 +99,12 @@ case "$1" in
     update-containers)
         atualizar_containers
         ;;
+    backup)
+        fazer_backup
+        ;;
     *)
+    
+
         echo "Uso: $0 {status|update|update-server|update-containers}"
         exit 1
         ;;
